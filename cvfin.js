520cb1568e5097f6857b613f0f7cc858b1a2133a
GustavoLTA <127359313+GustavoLTA@users.noreply.github.com>
2023-03-08 22:19:53 -0300

    Add files via upload

---
+++ cvfin.js
@@ -0,0 +1,16 @@
+
+
+
+
+document.getElementById('botondatospersonales').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'blue'; 
+    
+});
+
+
+
+document.getElementById('boton_color').addEventListener('click', function() {
+    document.body.style.backgroundColor = '#FF0000'; 
+    
+});
+
